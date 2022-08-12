ff6634bbcc28e737bf0b5be162c3d37b3fd68691
Gnanesh <gnaneshkunal@outlook.com>
2022-08-12 21:27:06 +0530

    Add latest resume and `use-package` to publish.el

--- publish.el
+++ publish.el
@@ -1,3 +1,16 @@
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
 (setq org-id-link-to-org-use-id
       'create-if-interactive-and-no-custom-id)
 
